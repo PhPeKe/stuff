2639106c5ff184752bf5951a177c89e96e3830ec
PhPeKe <p.kersten@live.de>
2018-09-28 16:34:06 +0200

    newest changes

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 bash check.sh & stdbuf -oL python start.py | tee output.txt
 rm output.txt
-
 wait
-
 echo "DONE!"
--- test.sh
+++ test.sh
@@ -4,21 +4,22 @@
 
 # Prerequirements: xdotools
 # Check for xdotool
-echo "Checking for xdotool"
+echo "checking for xdotool"
 dpkg -s xdotool &> /dev/null
 if [ $? == 0 ]
 then
-	echo "xdotool installed..."
-	grep -q '1' &> /dev/null
+	echo "xdotool installed"
+	grep -q 'Manager has exited with status code 1, restarting experiment...' &> /dev/null
 	if [ $? == 0 ] 
 	then
-	   echo "Found it"
+	   echo "restarting experiment..."
 	   xdotool keydown ctrl
 	   xdotool key d
 	   xdotool keyup ctrl
-	   python test2.py
+	   python start.py
+	   echo "restarted experiment"
 	fi
 else
-    echo "run: sudo apt install xdotool"
+    echo "try: sudo apt install xdotool"
 fi
 
